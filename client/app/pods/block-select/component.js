f7cca67b1e45222809b6589a0cd7a9f5f356ed28
lfarrell <lfarrell@email.unc.edu>
2017-10-17 11:54:25 -0400

    Fix issue with last select element always selected by default

--- client/app/pods/block-select/component.js
+++ client/app/pods/block-select/component.js
@@ -19,27 +19,27 @@ export default Ember.Component.extend(FocusEntryAction, {
   classNameBindings: ['required', 'invalid'],
   required: Ember.computed.alias('entry.required'),
   invalid: Ember.computed.alias('entry.invalid'),
-  
+
   didReceiveAttrs() {
     this._super(...arguments);
-    
+
     if (Ember.isBlank(this.get('entry.value'))) {
       if (this.get('entry.block.allowBlank')) {
         this.set('entry.value', this.get('entry.block.defaultValue') || '');
       } else {
         let firstOption = this.get('entry.block.options')[0];
         let firstValue = firstOption.value ? firstOption.value : firstOption;
-        
+
         this.set('entry.value', this.get('entry.block.defaultValue') || firstValue);
       }
     }
   },
-  
+
   actions: {
     focusEntry: function() {
       this.$('select').focus();
     },
-    
+
     change: function(value) {
       this.set('entry.value', value);
     }
