4c7b931f23fda267ec480a4659c4427efe6c6548
lfarrell <lfarrell@email.unc.edu>
2017-10-18 12:45:11 -0400

    Fix issue with december always the default selection for month precision dates

--- client/app/components/month-input.js
+++ client/app/components/month-input.js
@@ -15,18 +15,33 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['month-input'],
-  
+  monthList: [
+    {key:'', value: ''},
+    {key:'01', value: 'January'},
+    {key:'02', value: 'February'},
+    {key:'03', value: 'March'},
+    {key:'04', value: 'April'},
+    {key:'05', value: 'May'},
+    {key:'06', value: 'June'},
+    {key:'07', value: 'July'},
+    {key:'08', value: 'August'},
+    {key:'09', value: 'September'},
+    {key:'10', value: 'October'},
+    {key:'11', value: 'November'},
+    {key:'12', value: 'December'}
+  ],
+
   value: Ember.computed('year', 'month', {
     get() {
       let { year, month } = this.getProperties('year', 'month');
-      
+
       if (Ember.isEmpty(year) && Ember.isEmpty(month)) {
         return '';
       } else {
         return (year || '') + '-' + (month || '');
       }
     },
-    
+
     set(key, value) {
       if (Ember.typeOf(value) === 'string') {
         let [year, month] = value.split('-');
@@ -38,13 +53,13 @@ export default Ember.Component.extend({
       }
     }
   }),
-  
+
   actions: {
     setMonth: function(month) {
       this.set('month', month);
     },
     setYear: function(year) {
       this.set('year', year);
-    },
+    }
   }
 });
