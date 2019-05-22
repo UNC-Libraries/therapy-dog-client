74fc5561b5daf641eeb04a6dea8d2d382b75db96
lfarrell <lfarrell@email.unc.edu>
2019-05-22 13:01:39 -0400

    Switch CDR to DCR

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -16,7 +16,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   title: function(tokens) {
     tokens = Ember.makeArray(tokens);
-    tokens.unshift('Carolina Digital Repository');
+    tokens.unshift('Digital Collections Repository');
     return tokens.join(' - ');
   }
 });
